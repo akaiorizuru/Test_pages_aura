09ce548cf41d3557459f1061bd0ca00397abedfe
akaiorizuru <60643510+akaiorizuru@users.noreply.github.com>
2022-04-26 16:06:45 +0200

    test jquery

--- filter_type_activite.js
+++ filter_type_activite.js
@@ -13,4 +13,40 @@ $(document).ready(function(){
 
     });
 
+    // position "random"
+    // let a = 2;
+    let counterlength = $('.acteur_box').length;
+    let counternb = 0;
+    let maxNb = 15 ;
+    let minNb = -maxNb;
+    $('.acteur_box').css({margin:maxNb+1+"px"});
+    $('.title_type_activite').css({margin:maxNb+1+"px"});
+
+
+    for(let i = 0; i < counterlength; i++){
+        var randomNumberY =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
+        var randomNumberX =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
+
+        console.log(randomNumberY);
+
+        // if(counternb<=maxNb){ 
+        //     counternb = counternb+1;
+        //     if(counternb % 2 ==0){
+        //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
+        //     }else{
+        //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
+        //     }
+        // }
+        // else{
+        //     counternb =0;
+        // }
+
+        $('.acteur_box')[i].css({top:randomNumberY});
+        $('.acteur_box')[i].css({left:randomNumberX});
+
+
+    }
+
+    
+
 })
