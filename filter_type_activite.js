fa9878466e9547a1e545cbc4f8a1ed0b585f4ef1
akaiorizuru <60643510+akaiorizuru@users.noreply.github.com>
2022-04-26 15:08:13 +0200

    delete isotope filter test

--- filter_type_activite.js
+++ filter_type_activite.js
@@ -1,32 +1,5 @@
 $(document).ready(function(){
-    // $grid=$('.section_type_activite').isotope({
-	// 	itemSelector:'.acteur_box',
-	// 	masonry:{
-	// 		columnWidth: '.acteur_box'
-	// 	}
-	// })
-
-    // // pour gérer le filtrage par catégories
-
-	// $('.title_type_activite').on('click', function(event){
-	// 	var filterValue = $(this).attr('data-filter');
-
-	//   	$grid.isotope({ filter: filterValue });
-	//   	$('.title_type_activite').removeClass('active');
-	//   	$(this).addClass('active');
-	//   	event.preventDefault(); //avec le event entre parenthèse, il permet de rester sur la catégorie concerné
-	//   	console.log(filterValue);
-	// });
-    // $grid.isotope({ filter: '.conception:not(.transition)'});
-
-    // $('.title_type_activite').click(function grisee(){
-    //     $('.acteur_box, .conception').addClass('grisee');
-    // })   
-
-    // function grisee(){
-    //     $('.acteur_box, .conception').css({filter: "grayscale(1) blur(1px)"});
-    // }
-
+    //Système filtre
     $(".title_type_activite").click(function(e){
         let filter = "." + e.target.dataset.filter;
         //btn cliqué
