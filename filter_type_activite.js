b3a0538ce51d504726a2f9ee1cead26b68be917a
akaiorizuru <60643510+akaiorizuru@users.noreply.github.com>
2022-04-27 09:44:22 +0200

    find max bubble case inside each phase

--- filter_type_activite.js
+++ filter_type_activite.js
@@ -19,34 +19,60 @@ $(document).ready(function(){
     let counternb = 0;
     let maxNb = 15 ;
     let minNb = -maxNb;
-    $('.acteur_box').css({margin:maxNb+1+"px"});
-    $('.title_type_activite').css({margin:maxNb+1+"px"});
+    $('.acteur_box').css({margin:maxNb+1});
+    $('.title_type_activite').css({margin:maxNb+1});
 
 
-    for(let i = 0; i < counterlength; i++){
-        var randomNumberY =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
-        var randomNumberX =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
+    // Toutes les box
+    let boxes = $('.acteur_box');
+    console.log(boxes);
+    console.log($('acteur_box')[2])
 
-        console.log(randomNumberY);
+    // for(let i = 0; i < counterlength; i++){
+    //     var randomNumberY =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
+    //     var randomNumberX =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
 
-        // if(counternb<=maxNb){ 
-        //     counternb = counternb+1;
-        //     if(counternb % 2 ==0){
-        //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
-        //     }else{
-        //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
-        //     }
-        // }
-        // else{
-        //     counternb =0;
-        // }
-
-        $('.acteur_box')[i].css({top:randomNumberY});
-        $('.acteur_box')[i].css({left:randomNumberX});
+    //     console.log(randomNumberY);
 
 
-    }
+    //     // if(counternb<=maxNb){ 
+    //     //     counternb = counternb+1;
+    //     //     if(counternb % 2 ==0){
+    //     //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
+    //     //     }else{
+    //     //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
+    //     //     }
+    //     // }
+    //     // else{
+    //     //     counternb =0;
+    //     // }
 
+    //     boxes[i].css({"top":randomNumberY});
+    //     boxes[i].css({"left":randomNumberX});
+    // }
+
+    var maxBubbleValue = 0;
+    console.log(maxBubbleValue);
+    let bubble = $('.bubble');
+    console.log(bubble.length);
     
+    // $('.phase').each(function(){
+    //     val=$('.bubble').length;
+    //     console.log(val);
+    //     // if (maxBubbleValue <= val) {
+    //     //     maxBubbleValue = val;
+    //     // }
+    //     // console.log(maxBubbleValue);
+    // });
+    $( ".phase" ).each(function() {
+        val=$(this).find('.bubble').length;
+        // console.log($(this).find('.bubble'));
+        if (maxBubbleValue <= val) {
+            maxBubbleValue = val;
+        }
+        console.log(maxBubbleValue);
+    });
+
+    var nbPerSection=undefined;
 
 })
