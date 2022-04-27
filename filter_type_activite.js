9c7781e70a706a5e1da6d86ecd8876d66c442eba
akaiorizuru <60643510+akaiorizuru@users.noreply.github.com>
2022-04-27 16:57:24 +0200

    filter ok

--- filter_type_activite.js
+++ filter_type_activite.js
@@ -1,78 +1,64 @@
 $(document).ready(function(){
+    //var initialisation
+    let boxes = $('.acteur_box');
+
     //Système filtre
     $(".title_type_activite").click(function(e){
         let filter = "." + e.target.dataset.filter;
-        //btn cliqué
-        if($(this).hasClass('title_type_activite_notClicked')){
-            $(".title_type_activite"+filter).addClass('title_type_activite_clicked').removeClass('title_type_activite_notClicked');
+        const filterSelected = [];
+        var class_selected = "";
+        //Un bonton est cliqué alors : 
+        // si bouton possède la classe type_activite_notClicked (classe affecté aux éléments qui n'ont pas été cliqué ou qui ont été desélectionné)
+        if($(e.target).hasClass('title_type_activite_notClicked')){
+            $(e.target).addClass('title_type_activite_clicked').removeClass('title_type_activite_notClicked');
             $('.acteur_box').not(filter).addClass('grisee');
         }else{
-            $(".title_type_activite"+filter).addClass('title_type_activite_notClicked').removeClass('title_type_activite_clicked');  
-            $('.acteur_box').not(filter).removeClass('grisee');          
-        }
+            $(e.target).addClass('title_type_activite_notClicked').removeClass('title_type_activite_clicked'); 
+            $('.acteur_box').not(filter).removeClass('grisee');
+            $('.acteur_box').not(filter).addClass('CEDRIC');
 
+            $(".title_type_activite_clicked").each(function(i,element) {
+                var value_data_filter = element.getAttribute('data-filter');
+                $('.acteur_box').not('.' + value_data_filter).addClass("grisee");
+            });
+        }  
     });
 
-    // position "random"
-    // let a = 2;
-    let counterlength = $('.acteur_box').length;
-    let counternb = 0;
-    let maxNb = 15 ;
-    let minNb = -maxNb;
-    $('.acteur_box').css({margin:maxNb+1});
-    $('.title_type_activite').css({margin:maxNb+1});
-
-
-    // Toutes les box
-    let boxes = $('.acteur_box');
-    console.log(boxes);
-    console.log($('acteur_box')[2])
-
-    // for(let i = 0; i < counterlength; i++){
-    //     var randomNumberY =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
-    //     var randomNumberX =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
-
-    //     console.log(randomNumberY);
-
-
-    //     // if(counternb<=maxNb){ 
-    //     //     counternb = counternb+1;
-    //     //     if(counternb % 2 ==0){
-    //     //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
-    //     //     }else{
-    //     //         $('.acteur_box')[i].css({top:5+counternb*2+"px"});
-    //     //     }
-    //     // }
-    //     // else{
-    //     //     counternb =0;
-    //     // }
-
-    //     boxes[i].css({"top":randomNumberY});
-    //     boxes[i].css({"left":randomNumberX});
-    // }
-
+    //Récupérer le nombre de "bulles" total maximum d'une section "phase"
+    //initialisation de valeur
     var maxBubbleValue = 0;
     console.log(maxBubbleValue);
     let bubble = $('.bubble');
     console.log(bubble.length);
     
-    // $('.phase').each(function(){
-    //     val=$('.bubble').length;
-    //     console.log(val);
-    //     // if (maxBubbleValue <= val) {
-    //     //     maxBubbleValue = val;
-    //     // }
-    //     // console.log(maxBubbleValue);
-    // });
     $( ".phase" ).each(function() {
         val=$(this).find('.bubble').length;
         // console.log($(this).find('.bubble'));
-        if (maxBubbleValue <= val) {
+        if (maxBubbleValue < val) {
             maxBubbleValue = val;
         }
         console.log(maxBubbleValue);
     });
+    
 
-    var nbPerSection=undefined;
+    // Générer un position aléatoire des "acteur_box"
+    // Initialisation de valeur
+    let maxNb = 85 ;
+    let minNb = -maxNb;
+    $('.acteur_box').css({"margin":maxNb/2});
+    $('.title_type_activite').css({"margin":maxNb/2});
+    // console.log(boxes);
+    // console.log($('.acteur_box')[2]);
+    // console.log(boxes[2])
+        
+    boxes.each(function(i) {
+        // génération de nombre aléatoire avec un minimum et maximum
+        var randomNumberY =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
+        var randomNumberX =  Math.floor(Math.random() * (maxNb - minNb + 1)) + minNb;
+        // console.log(boxes[i]);
+        // change de la position top et left de base
+        $(this).css({"top":randomNumberY});
+        $(this).css({"left":randomNumberX});
+    });
 
 })
